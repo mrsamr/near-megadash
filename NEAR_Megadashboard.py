9a3870d96a17ddc46c726f4888c35bd1ddf1827d
mrsamr <rmas.eth11@gmail.com>
2023-01-05 03:58:59 +0800

    Added dashboard intro

--- NEAR_Megadashboard.py
+++ NEAR_Megadashboard.py
@@ -3,6 +3,29 @@ import streamlit as st
 st.set_page_config(layout='wide')
 st.title('NEAR Mega Dashboard')
 
-st.text('This section explains what this dashboard is about.')
+
+intro_text = r"""
+This app contains information and metrics on various parts of the NEAR ecosystem including:
+- Blockchain activity
+- Blockchain performance
+- Staking and validators
+- Decentralized finance
+
+Please navigate to the different pages to view the information listed above.
+
+---
+
+**Data sources:**
+1. Flipside Crypto
+2. DefiLlama
+3. NEAR RPC Node
+
+---
+
+*This app was originally developed as a contribution to MetricsDAO.*
+
+"""
+
+st.markdown(intro_text)
 
 
